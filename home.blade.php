ade11f01a9818e9f14f34ac319098765bf2cff40
meeravachhani <meeravachhani02@gmail.com>
2025-02-07 09:18:50 +0530

    Create home.blade.php

---
+++ home.blade.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="/asets/css/mytext.css">
+    <titlt> </title>
+
+</head>
+
+    <body>
+        
+<nav class="navbar navbar-expand-sm bg-light">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="about">aboute</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus"> contact us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="service">service </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        <h1>welcome to atmiya university</h1>
+        <p>goodmorning</p>
+<center>
+        <form action="/submitdata" method="post">
+            @csrf
+            <input type="text"  name="username" placeholder="enter your name">
+            <input type="password"  name="password" placeholder="enter your passwoedame">
+            <input type="submit"  value="submit" >
+            
+
+        </form>
+</center>
+        <img src="/asets/img/Untitled.png" alt="imges" height="300px">
+        <p class="mytext"> hii
+            hello
+        </p>
+</body>
+    </html>
